955cd0a7b44745dae3eb563dd38a116d0ccfff9f
Mark Krawczuk <mkrawczuk@tw-mbp-mkrawczuk.attlocal.net>
2016-12-16 16:56:08 -0800

    Added a tableview and working on second screen

--- ViewController.swift
+++ ViewController.swift
@@ -4,17 +4,40 @@
 //
 //  Created by Mark Krawczuk on 12/16/16.
 //  Copyright © 2016 Mark Krawczuk. All rights reserved.
-//
+//"😊😎😳😂😏🤓😀😜"
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var dacooltableview: UITableView!
+
+    var emojis = ["😊","😎","😳","😂","😏","🤓"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        dacooltableview.dataSource = self
+        dacooltableview.delegate = self
+        
+        
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
